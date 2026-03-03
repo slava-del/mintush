c03b6d41df719ab3d40a6c3737e5ca5bdcbe2c11
slava-del <slavaiurco1@gmail.com>
2026-03-04 00:01:53 +0200

    Restrict public routes to education and business strategy

---
+++ proxy.ts
@@ -0,0 +1,31 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+const ALLOWED_PATHS = new Set([
+  "/",
+  "/education",
+  "/education/business-strategy",
+  "/__404__",
+]);
+
+function normalizePathname(pathname: string) {
+  if (pathname.length > 1 && pathname.endsWith("/")) {
+    return pathname.slice(0, -1);
+  }
+  return pathname;
+}
+
+export function proxy(request: NextRequest) {
+  const pathname = normalizePathname(request.nextUrl.pathname);
+
+  if (ALLOWED_PATHS.has(pathname)) {
+    return NextResponse.next();
+  }
+
+  const url = request.nextUrl.clone();
+  url.pathname = "/__404__";
+  return NextResponse.rewrite(url);
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)"],
+};
